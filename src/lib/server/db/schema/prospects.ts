b07798578432075084a85c7c384c68b02647bbb7
Clayton <254567671+desertrambler10@users.noreply.github.com>
2026-01-28 18:31:24 -0600

    modified persistence layer for prospect with not nulls

--- src/lib/server/db/schema/prospects.ts
+++ src/lib/server/db/schema/prospects.ts
@@ -4,8 +4,8 @@ import { pgTable, text, serial } from 'drizzle-orm/pg-core';
 export const prospect = pgTable('prospect', {
 	id: serial('id').primaryKey(),
 	name: text('name').notNull(),
-	email: text('email'),
-	message: text('message')
+	email: text('email').notNull(),
+	message: text('message').notNull()
 });
 
 export type NewProspectRow = typeof prospect.$inferInsert;
